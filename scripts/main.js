11902844ab2f58fcd076e304e5a784ea9ba95788
Patricia Kusumaningtyas <patricia.adiyoso@gmail.com>
2020-10-22 21:10:03 -0400

    Added info in about and experiences page

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+Vue.component('tagline', {
+    template: `
+        <h1>Patricia Kusumaningtyas</h1>
+        <h3>Software Engineer, Educator, Writer</h3>
+    `
+})
+
 Vue.component('menu-buttons', {
     template: `
     <button v-for="tab in tabs" @click="selectedTab = tab"> {{ tab }} </button>
@@ -28,19 +35,47 @@ Vue.component('menu-buttons', {
 
 Vue.component('about-page', {
     template: `
-    <p>About page</p>
+    <table id="about">
+        <tr>
+            <td>
+                <img src="scripts/resources/selfphoto.png" />
+            </td>
+            <td>
+                <div id="bio">
+                <p>I'm a junior software engineer based in Brooklyn, NY. </p>
+                <p>Born and raised in Jakarta, Indonesia, I moved to the United States to earn my associate's degree in computer science from 
+                Green River College (Auburn, Washington) in 2018 and my bachelor's degree in computer science from Columbia University 
+                (New York, New York) in 2020. I currently work at <a href="http://www.jri-america.com/">JRI America</a>; previously,
+                I was a software engineer intern at <a href="https://www.gojek.com/">Gojek</a> in 2019 and a graphic design intern at 
+                <a href="https://korporasi.kompas.id/">Kompas</a> in 2015.</p>
+                <p>Besides hacking, coding, and squashing bugs, I am also passionate in education and writing. In my college career, I was
+                a writing consultant at both Columbia and Green River, and I continued my passion post-graduation by volunteering in 
+                organizations such as <a href="https://www.btny.org/">Breakthrough New York</a> and <a href="https://www.girlsclub.org/">
+                the Lower East Side Girls Club</a>. I also publish my own pieces through various media publications; my works 
+                have been published in websites such as <a href="https://www.whiteboardjournal.com/">Whiteboard Journal</a> and
+                <a href="https://magdalene.co/">Magdalene</a>.</p>
+                <p>In my spare time, I like to listen to music, cook, and dismantle the bourgeoisie.</p>
+                </div>
+            </td>
+        </tr>
+    </table>
     `
 });
 
 Vue.component('experience-page', {
     template: `
-    <p>Experience page</p>
-    `
+    <h3>Education</h3>
+    <ul>
+        <li v-for="education in educationHistory">
+        </li>
+    </ul>
+    `,
+    data: experienceData()
 });
 
 Vue.component('portfolio-page', {
     template: `
-    <p>Portfolio page</p>
+    <p>Coming soon!</p>
     `
 });
 
